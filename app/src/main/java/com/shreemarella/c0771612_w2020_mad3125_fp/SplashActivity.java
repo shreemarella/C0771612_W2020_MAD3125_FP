4d9c7daae5a08356c645cf16e37f9292d86a5443
shreemarella <mvsrikanth96@gmail.com>
2020-04-17 12:46:32 -0400

    [SM] designed splash screen followinf to login activity

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/SplashActivity.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/SplashActivity.java
@@ -2,13 +2,26 @@ package com.shreemarella.c0771612_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity
+{
+    private int SPLASH_TIMING = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent hint = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(hint);
+                finish();
+            }
+        },SPLASH_TIMING);
+    }
     }
 }
