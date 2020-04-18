eee4065757ae622fdcf41bfdbbcb1fb31e1b3331
shreemarella <mvsrikanth96@gmail.com>
2020-04-17 20:03:41 -0400

    [SM] created login screen layout

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/LoginActivity.java
@@ -3,12 +3,26 @@ package com.shreemarella.c0771612_w2020_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
+//    private EditText edtEmail;
+//    private EditText edtPassword;
+//    private Switch swtRemember;
+//    private Button btnLogin;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//        edtEmail=findViewById(R.id.edtEmail);
+//        edtPassword=findViewById(R.id.edtPassword);
+//        swtRemember=findViewById(R.id.swtRemember);
+//        btnLogin=findViewById(R.id.btnLogin);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
     }
 }
