f441e26a49ce7c72f401ac22a70a5b8426b508cd
shreemarella <mvsrikanth96@gmail.com>
2020-04-17 22:05:57 -0400

    [SM] customers array created

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/customers_list.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/customers_list.java
@@ -1,16 +1,37 @@
 package com.shreemarella.c0771612_w2020_mad3125_fp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.shreemarella.c0771612_w2020_mad3125_fp.R;
 
-public class customers_list extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class customers_list extends AppCompatActivity
+{
+    private RecyclerView rvCustomerList;
+    private ArrayList<customer> customers;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customers_list);
+        rvCustomerList=findViewById(R.id.rvcustomerlist);
+
+        customersInfo();
+    }
+    protected void onRestart() {
+
+        super.onRestart();
+
+        customersInfo();
+    }
+
+    private void customersInfo()
+    {
+
     }
 }
