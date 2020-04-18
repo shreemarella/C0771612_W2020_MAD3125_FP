6af93c30aef59ceaf412a25c1a08d15417f40fac
shreemarella <mvsrikanth96@gmail.com>
2020-04-17 22:39:48 -0400

    [SM]customer class HashMap

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/classes/customer.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/classes/customer.java
@@ -16,55 +16,23 @@ public class customer implements Parcelable
     private HashMap<String, Bill> customerBills = new HashMap<>();
     private Double allTotal;
 
-//    public customer(String customerId, String firstName, String lastName, String emailId) {
-//        this.customerId = customerId;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.emailId = emailId;
-//    }
-//
-//
-//    protected customer(Parcel in)
-//    {
-//        customerId = in.readString();
-//        firstName = in.readString();
-//        lastName = in.readString();
-//        emailId = in.readString();
-//        customerBills = in.readHashMap(Bill.class.getClassLoader());
-//        allTotal = in.readDouble();
-//    }
-//
-//    public String getCustomerId() {
-//        return customerId;
-//    }
-//
-//    public void setCustomerId(String customerId) {
-//        this.customerId = customerId;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getEmailId() {
-//        return emailId;
-//    }
-//
-//    public void setEmailId(String emailId) {
-//        this.emailId = emailId;
-//    }
+    public customer(String customerId, String firstName, String lastName, String emailId) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+
+    protected customer(Parcel in)
+    {
+        customerId = in.readString();
+        firstName = in.readString();
+        lastName = in.readString();
+        emailId = in.readString();
+        customerBills = in.readHashMap(Bill.class.getClassLoader());
+        allTotal = in.readDouble();
+    }
 
     public static final Creator<customer> CREATOR = new Creator<customer>() {
         @Override
@@ -77,6 +45,52 @@ public class customer implements Parcelable
             return new customer[size];
         }
     };
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    public void addBill(String billId, Bill bill)
+    {
+        this.customerBills.put(billId, bill);
+    }
+
+    public HashMap<String, Bill> getCustomerBills() {
+        return customerBills;
+    }
+
+    public void setCustomerBills(HashMap<String, Bill> customerBills) {
+        this.customerBills = customerBills;
+    }
+
+
 
     @Override
     public int describeContents() {
