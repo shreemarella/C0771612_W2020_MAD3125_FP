6b1320794c439a45007b45766c22e44da56d4540
shreemarella <mvsrikanth96@gmail.com>
2020-04-17 20:04:17 -0400

    [SM] created login screen activity

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/LoginActivity.java
@@ -9,20 +9,20 @@ import android.widget.Switch;
 
 public class LoginActivity extends AppCompatActivity
 {
-//    private EditText edtEmail;
-//    private EditText edtPassword;
-//    private Switch swtRemember;
-//    private Button btnLogin;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
-//        edtEmail=findViewById(R.id.edtEmail);
-//        edtPassword=findViewById(R.id.edtPassword);
-//        swtRemember=findViewById(R.id.swtRemember);
-//        btnLogin=findViewById(R.id.btnLogin);
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Switch swtRemember;
+    private Button btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edtEmail=findViewById(R.id.edtEmail);
+        edtPassword=findViewById(R.id.edtPassword);
+        swtRemember=findViewById(R.id.swtRemember);
+        btnLogin=findViewById(R.id.btnLogin);
 
     }
 }
