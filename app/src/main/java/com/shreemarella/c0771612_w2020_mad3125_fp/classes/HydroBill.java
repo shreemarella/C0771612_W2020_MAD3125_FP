4d94ed64a48d337b4edec30dc1d2e8d8b921dd0d
shreemarella <mvsrikanth96@gmail.com>
2020-04-17 23:36:12 -0400

    [SM] hydro bill added

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/classes/HydroBill.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/classes/HydroBill.java
@@ -3,9 +3,9 @@ package com.shreemarella.c0771612_w2020_mad3125_fp.classes;
 public class HydroBill extends Bill implements IDisplay
 {
     private String agencyName;
-    private String unitsConsumed;
+    private int unitsConsumed;
 
-    public HydroBill(String billId, String billDate, String billType, Double totalBillAmount, String agencyName, String unitsConsumed) {
+    public HydroBill(String billId, String billDate, String billType, Double totalBillAmount, String agencyName, int unitsConsumed) {
         super(billId, billDate, billType, totalBillAmount);
         this.agencyName = agencyName;
         this.unitsConsumed = unitsConsumed;
@@ -19,11 +19,11 @@ public class HydroBill extends Bill implements IDisplay
         this.agencyName = agencyName;
     }
 
-    public String getUnitsConsumed() {
+    public int getUnitsConsumed() {
         return unitsConsumed;
     }
 
-    public void setUnitsConsumed(String unitsConsumed) {
+    public void setUnitsConsumed(int unitsConsumed) {
         this.unitsConsumed = unitsConsumed;
     }
 
--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/repo/customerRepository.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/repo/customerRepository.java
@@ -1,5 +1,8 @@
 package com.shreemarella.c0771612_w2020_mad3125_fp.repo;
 
+import com.shreemarella.c0771612_w2020_mad3125_fp.classes.HydroBill;
+import com.shreemarella.c0771612_w2020_mad3125_fp.classes.InternetBill;
+import com.shreemarella.c0771612_w2020_mad3125_fp.classes.MobileBill;
 import com.shreemarella.c0771612_w2020_mad3125_fp.classes.customer;
 
 import java.util.ArrayList;
@@ -24,6 +27,11 @@ public class customerRepository
             }
             public void loadDetails()
             {
-                
+                HydroBill H1 = new HydroBill("HYD001","15/10/1996","HYDRO",200.50,"powertoronto",45);
+//                MobileBill M1 = new MobileBill("MOB001","10/12/1998","MOBILE",325.89,"Apple INC","2.5Gb and Unlimited calls","+18199210696",25,890);
+//                InternetBill I1 = new InternetBill("INT001","03/08/2001","INTERNET",456.78,"Rogers","256.78");
+//                customer customer1 = new customer("C0001","Shree","Marella","Shree@gmail.com");
+//                customer customer2 = new customer()
+
             }
 }
