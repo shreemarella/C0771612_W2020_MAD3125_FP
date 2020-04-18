54203cbbea39331f5d8bf57ce76ddc6530a0f4c4
shreemarella <mvsrikanth96@gmail.com>
2020-04-17 22:54:09 -0400

    [SM]hydro Bill class variables with constructor

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/classes/HydroBill.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/classes/HydroBill.java
@@ -1,6 +1,15 @@
 package com.shreemarella.c0771612_w2020_mad3125_fp.classes;
 
-public class HydroBill
+public class HydroBill extends Bill implements IDisplay
 {
+    private String agencyName;
+    private String unitsConsumed;
+
+    public HydroBill(String billId, String billDate, String billType, Double totalBillAmount, String agencyName, String unitsConsumed) {
+        super(billId, billDate, billType, totalBillAmount);
+        this.agencyName = agencyName;
+        this.unitsConsumed = unitsConsumed;
+    }
+
     
 }
