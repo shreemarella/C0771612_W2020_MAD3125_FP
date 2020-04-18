57b4963423f718b5fedee2cb7ee20e85488aafc3
shreemarella <mvsrikanth96@gmail.com>
2020-04-17 22:12:01 -0400

    [SM] Bill class getter setter done

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/classes/Bill.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/classes/Bill.java
@@ -12,5 +12,38 @@ public class Bill
         this.billDate = billDate;
         this.billType = billType;
         this.totalBillAmount = totalBillAmount;
+
+    }
+
+    public String getBillId() {
+        return billId;
+    }
+
+    public void setBillId(String billId) {
+        this.billId = billId;
+    }
+
+    public String getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(String billDate) {
+        this.billDate = billDate;
+    }
+
+    public String getBillType() {
+        return billType;
+    }
+
+    public void setBillType(String billType) {
+        this.billType = billType;
+    }
+
+    public String getTotalBillAmount() {
+        return totalBillAmount;
+    }
+
+    public void setTotalBillAmount(String totalBillAmount) {
+        this.totalBillAmount = totalBillAmount;
     }
 }
