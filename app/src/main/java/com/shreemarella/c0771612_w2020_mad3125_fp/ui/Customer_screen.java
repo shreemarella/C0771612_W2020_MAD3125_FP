740e778bbb6d2bb6179ffd5ff6f7da42b0d006b3
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 14:38:59 -0400

    [SM] customer screen activity

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/Customer_screen.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/Customer_screen.java
@@ -1,18 +1,34 @@
 package com.shreemarella.c0771612_w2020_mad3125_fp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.shreemarella.c0771612_w2020_mad3125_fp.R;
+import com.shreemarella.c0771612_w2020_mad3125_fp.customeradapter.billsadapter;
+
+import java.util.ArrayList;
 
 public class Customer_screen extends AppCompatActivity
 {
-
+    private TextView CustomerId;
+    private TextView FullName;
+    private TextView Email;
+    private TextView TotalAmountToPay;
+    private RecyclerView rvBills;
+    private billsadapter Billsadapter;
+    private ArrayList billsArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_screen);
+        CustomerId = findViewById(R.id.txtCustomerId);
+        FullName = findViewById(R.id.txtCustomerName);
+        Email = findViewById(R.id.txtCustomerEmail);
+        TotalAmountToPay = findViewById(R.id.txtCustomerTotalAmount);
+        rvBills=findViewById(R.id.rvbills);
     }
 }
