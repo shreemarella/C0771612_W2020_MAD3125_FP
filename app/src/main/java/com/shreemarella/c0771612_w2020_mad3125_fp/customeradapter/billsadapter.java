ae4d6557deb8e955b7d63fc3b74be13a57bd4a17
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 12:40:41 -0400

    [SM] Bills adapter recycler view implemented

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/customeradapter/billsadapter.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/customeradapter/billsadapter.java
@@ -1,7 +1,14 @@
 package com.shreemarella.c0771612_w2020_mad3125_fp.customeradapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.shreemarella.c0771612_w2020_mad3125_fp.R;
 import com.shreemarella.c0771612_w2020_mad3125_fp.classes.Bill;
 
 import java.util.ArrayList;
@@ -18,6 +25,47 @@ public class billsadapter extends RecyclerView.Adapter<billsadapter.ViewHolder>
     {
 
     }
-    
+
+    @NonNull
+    @Override
+    public billsadapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.bill_add, parent, false);
+        billsadapter.ViewHolder mBillsViewHolder = new billsadapter.ViewHolder(mView);
+        return mBillsViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull billsadapter.ViewHolder holder, int position) {
+        Bill mBills = this.billArrayList.get(position);
+        holder.txtCellBillId.setText(mBills.getBillId());
+        holder.txtCellBillAmount.setText(mBills.getTotalBillAmount().toString());
+        holder.txtCellBillDate.setText(mBills.getBillDate().toString());
+        holder.txtCellBillType.setText(mBills.getBillType().toString());
+
+    }
+    @Override
+    public int getItemCount()  {
+        return this.billArrayList.size();
+
+    }
+
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//
+//        TextView txtCellBillId;
+//        TextView txtCellBillDate;
+//        TextView txtCellBillType;
+//        TextView txtCellBillAmount;
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            txtCellBillId = itemView.findViewById(R.id.lblCellId);
+//            txtCellBillDate = itemView.findViewById(R.id.txtCellBillDate);
+//            txtCellBillType = itemView.findViewById(R.id.txtCellBillType);
+//            txtCellBillAmount = itemView.findViewById(R.id.txtCellBillAmount);
+//
+//        }
+//
+//    }
+
 
 }
