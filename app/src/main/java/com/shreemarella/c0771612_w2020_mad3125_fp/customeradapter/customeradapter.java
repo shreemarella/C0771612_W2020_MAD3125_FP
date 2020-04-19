a10022118ce04202655f3a18190f4bb85875330f
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 14:03:12 -0400

    [SM] customer adapter view holder

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/customeradapter/customeradapter.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/customeradapter/customeradapter.java
@@ -1,4 +1,27 @@
 package com.shreemarella.c0771612_w2020_mad3125_fp.customeradapter;
 
-public class customeradapter {
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.shreemarella.c0771612_w2020_mad3125_fp.R;
+import com.shreemarella.c0771612_w2020_mad3125_fp.classes.customer;
+import com.shreemarella.c0771612_w2020_mad3125_fp.repo.customerRepository;
+
+public class customeradapter extends RecyclerView.Adapter<customeradapter.customerviewholder>
+{
+    @NonNull
+    @Override
+    public customeradapter.customerviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.listcustomers, parent, false);
+        customerviewholder mCustomerViewHolder = new customerviewholder(mView);
+        return mCustomerViewHolder;
+    }
+
+
 }
