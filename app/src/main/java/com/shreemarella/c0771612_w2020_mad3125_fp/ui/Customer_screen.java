76414fd27b6287a224d90054cd9a77811833a4d8
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 14:14:20 -0400

    [SM] created customers screen activity

---
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/Customer_screen.java
@@ -0,0 +1,18 @@
+package com.shreemarella.c0771612_w2020_mad3125_fp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shreemarella.c0771612_w2020_mad3125_fp.R;
+
+public class Customer_screen extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_screen);
+    }
+}
