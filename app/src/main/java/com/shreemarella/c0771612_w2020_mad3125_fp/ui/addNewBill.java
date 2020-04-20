1e05f0800e83471f1b37837d4a0fb45c1096234b
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 23:09:47 -0400

    [SM]date formatter added to add new bill activity

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/addNewBill.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/addNewBill.java
@@ -3,17 +3,23 @@ package com.shreemarella.c0771612_w2020_mad3125_fp.ui;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.shreemarella.c0771612_w2020_mad3125_fp.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class addNewBill extends AppCompatActivity implements  AdapterView.OnItemSelectedListener
 {
@@ -60,6 +66,38 @@ public class addNewBill extends AppCompatActivity implements  AdapterView.OnItem
         spbillType.setAdapter(dataAdapter);
 
         spbillType.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        final Calendar myCalendar = Calendar.getInstance();
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String myFormat = "MM/dd/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                billDate.setText(sdf.format(myCalendar.getTime()));
+                billDate.setTextColor(Color.BLACK);
+
+            }
+
+        };
+
+        billDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(addNewBill.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+
+        });
     }
 
     @Override
