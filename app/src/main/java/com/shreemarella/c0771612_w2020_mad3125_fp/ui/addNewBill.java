8a167d9b37c51f89a57c6d2463a10ccfda81c811
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 22:59:17 -0400

    [SM] Bill type arraylist added

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/addNewBill.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/addNewBill.java
@@ -1,16 +1,74 @@
 package com.shreemarella.c0771612_w2020_mad3125_fp.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.shreemarella.c0771612_w2020_mad3125_fp.R;
 
-public class addNewBill extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class addNewBill extends AppCompatActivity implements  AdapterView.OnItemSelectedListener
+{
+    private TextView billID;
+    private TextView billDate;
+    private TextView unitsUsed;
+    private TextView agencyName;
+    private TextView minutesUsed;
+    private TextView manufacturerName;
+    private TextView planName;
+    private Spinner spbillType;
+    private TextView mobilenumber;
+    private TextView dataused;
+    private Button btnAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_bill);
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.hide();
+
+        btnAdd=findViewById(R.id.btnAdd);
+        billID=findViewById(R.id.BillIDTextInputEditText);
+        billDate = findViewById(R.id.BillDateTextInputEditText);
+        unitsUsed = findViewById(R.id.enterunitsusedInputEditText);
+        agencyName = findViewById(R.id.enteragencynameInputEditText);
+        minutesUsed = findViewById(R.id.enterminutesusedInputEditText);
+        manufacturerName = findViewById(R.id.entermanufacturernameInputEditText);
+        planName = findViewById(R.id.enteryourplannameInputEditText);
+        mobilenumber = findViewById(R.id.mobilenumberInputEditText);
+        dataused = findViewById(R.id.datausedInputEditText);
+        spbillType = findViewById(R.id.spinnertype);
+
+        ArrayList<String> billType = new ArrayList<>();
+        billType.add("MOBILE");
+        billType.add("HYDRO");
+        billType.add("INTERNET");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, billType);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spbillType.setAdapter(dataAdapter);
+
+        spbillType.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
