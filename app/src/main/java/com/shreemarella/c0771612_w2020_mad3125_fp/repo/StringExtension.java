a52de8966dc4c2c1cb41a827331749a69f205fb7
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 22:24:44 -0400

    [SM] mobile validation function added

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/repo/StringExtension.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/repo/StringExtension.java
@@ -2,5 +2,11 @@ package com.shreemarella.c0771612_w2020_mad3125_fp.repo;
 
 public class StringExtension
 {
+    public static boolean mobileValidation(String s)
+    {
+        if(s.matches("\\d{10}")) return true;
+        else return false;
+    }
+    
 
 }
