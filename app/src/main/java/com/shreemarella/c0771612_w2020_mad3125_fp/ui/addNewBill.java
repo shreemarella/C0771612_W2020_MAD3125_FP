25d30399893b456a8ba74a45b9fd139fb144d02c
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 22:33:19 -0400

    [sm] add new bill activity created and designed layout

---
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/ui/addNewBill.java
@@ -0,0 +1,16 @@
+package com.shreemarella.c0771612_w2020_mad3125_fp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shreemarella.c0771612_w2020_mad3125_fp.R;
+
+public class addNewBill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_bill);
+    }
+}
