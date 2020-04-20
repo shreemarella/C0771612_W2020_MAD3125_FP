9acd7076d106c089bc141eb6bc947e8693a36932
shreemarella <mvsrikanth96@gmail.com>
2020-04-19 22:26:11 -0400

    [SM] Email validation function added

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/repo/StringExtension.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_fp/repo/StringExtension.java
@@ -7,6 +7,12 @@ public class StringExtension
         if(s.matches("\\d{10}")) return true;
         else return false;
     }
+
+    public static boolean emailValidation(String s)
+    {
+        if (s.matches("[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"))  return true;
+        else return false;
+    }
     
 
 }
